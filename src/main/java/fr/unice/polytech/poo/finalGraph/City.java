33640bb2818a61cd66e03a75f9602de021504af2
Fabien Airaud <fabien.airaud@etu.unice.fr>
2023-06-20 16:50:54 +0200

    Ajout classe villes

---
+++ src/main/java/fr/unice/polytech/poo/finalGraph/City.java
@@ -0,0 +1,51 @@
+package fr.unice.polytech.poo.finalGraph;
+
+import java.util.Objects;
+
+public record City(String nom, double latitude, double longitude) {
+
+    /**
+     * Calcule la latitude en radian
+     * @return la latitude en radian
+     */
+    public double radLat() {
+        return latitude * (Math.PI / 180);
+    }
+
+    /**
+     * Calcule la longitude en radian
+     * @return la longitude en radian
+     */
+    public double radLon() {
+        return longitude * (Math.PI / 180);
+    }
+
+    /**
+     * Calcule la distance séparant la ville actuelle à la ville de destination
+     * @param dest la ville de destination
+     * @return la distance en mètre entre les deux villes
+     */
+    public double distance(City dest) {
+        return 6378137 * Math.acos(Math.sin(this.radLat()) * Math.sin(dest.radLat())
+                + Math.cos(this.radLat()) * Math.cos(dest.radLat()) * Math.cos(dest.radLon() - this.radLon()));
+    }
+
+    @Override
+    public String toString() {
+        return nom + " (" + latitude +
+                "°," + longitude + "°)";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Double.compare(city.latitude, latitude) == 0 && Double.compare(city.longitude, longitude) == 0 && Objects.equals(city.nom, nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, latitude, longitude);
+    }
+}
